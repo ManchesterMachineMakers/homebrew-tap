a7ece276b554ba46187162e83e9eaf0c16ec07f2
Aleks Rūtiņš <aleks@rutins.com>
2022-03-13 14:25:00 -0400

    Remove the shasum

--- get-robot-code.rb
+++ get-robot-code.rb
@@ -6,7 +6,7 @@ class GetRobotCode < Formula
     homepage ""
     version "0.1.0"
     url "file:///dev/null"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 ""
     license ""
 
     depends_on "wget"
