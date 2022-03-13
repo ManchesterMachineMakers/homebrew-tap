14acef59fc74ced4409b1d1249eb6bb94a5bd4c3
Aleks Rūtiņš <aleks@rutins.com>
2022-03-13 14:20:06 -0400

    Add a shasum

--- get-robot-code.rb
+++ get-robot-code.rb
@@ -6,7 +6,7 @@ class GetRobotCode < Formula
     homepage ""
     version "0.1.0"
     url "file:///dev/null"
-    sha256 ""
+    sha256 "a948904f2f0f479b8f8197694b30184b0d2ed1c1cd2a1ec0fb85d299a192a447"
     license ""
 
     depends_on "wget"
